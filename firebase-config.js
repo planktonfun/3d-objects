2493c62432888df437280ab6ca021766ab9c1874
planktonfun <paulo.castro48+github@gmail.com>
2026-04-12 01:47:06 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+      // Firebase configuration - leave blank to work offline
+const firebaseConfig = {
+        apiKey: "AIzaSyAH2iXY79IOKr-ssC6hV1cfUho-_oKJgCg",
+        authDomain: "lime-for-group-1567103664368.firebaseapp.com",
+        databaseURL: "https://lime-for-group-1567103664368.firebaseio.com",
+        projectId: "lime-for-group-1567103664368",
+        storageBucket: "lime-for-group-1567103664368.appspot.com",
+        messagingSenderId: "118271867075",
+        appId: "1:118271867075:web:2847b66a50511c3cea4a76",
+        measurementId: "G-NYBTWV1MBT"
+    };
+      const FIREBASE_PREFIX = 'sandbox';
